d07a98b530efe2ed31ebadea248c9ae55a69dc30
Damir Mustafin <damir.mustafin@protonmail.ch>
2021-03-26 13:28:31 +0300

    kloc

---
+++ README.md
@@ -0,0 +1,22 @@
+KLOC
+==
+
+> Get [kloc](https://www.webopedia.com/definitions/kloc/) for a software program.
+
+## usage
+
+### cli
+
+```bash
+...
+```
+
+### nodejs
+
+```js
+...
+```
+
+## license
+
+MIT
---
+++ cli.js
@@ -0,0 +1,34 @@
+const meow = require('meow')
+
+const get_kloc = require('./')
+
+const cli = meow(`
+
+  Usage:
+
+    $ kloc <project_path>
+
+  Options
+
+    --exclude     Files & dirs to exclude (supports nested path)
+    --show-logs   Include logs in the output
+
+  Usage
+
+    $ kloc ~/code/my-app
+    $ kloc ~/code/my-app --exclude .git,node_modules,dist --show-logs
+
+  `)
+
+if (cli.input.length > 0 && cli.input[0] !== '') {
+    const project_path = cli.input[0]
+    const { exclude, showLogs } = cli.flags
+
+    get_kloc(project_path, { exclude, show_logs: showLogs })
+      .then(kloc => console.log(`\nKLOC IN PROJECT: ${kloc}.`))
+      .catch(err => console.error(err.message))
+  } else {
+    console.log(cli.help)
+
+    process.exit(1)
+  }
